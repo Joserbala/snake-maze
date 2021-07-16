bcb4c4ae98975f12d3c6abc18c493c954ed7f1d8
José Gala Naranjo <jgalanar@gmail.com>
2021-07-16 20:46:33 +0200

    Work at loading snake

--- Assets/Scripts/UI/DoLerp.cs
+++ Assets/Scripts/UI/DoLerp.cs
@@ -1,16 +1,51 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace SnakeMaze.UI
 {
     public class DoLerp : MonoBehaviour
     {
-        [SerializeField] private float timeToLerp = 1.0f;
+        [SerializeField] private float unitsPerSecondInitial = 1.0f;
+        [SerializeField] private float unitsPerSecondFinal = 2.0f;
+        [SerializeField] private float minTimeToLerp = .5f;
+        [SerializeField] private float maxTimeToLerp = 1.0f;
+        [SerializeField] private float stoppedTime = .1f;
 
-        private void Update()
+        private float unitsPerSecond;
+        private float timeToLerp;
+
+        private void Start()
         {
-            gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, Quaternion.identity, timeToLerp * Time.deltaTime);
+            unitsPerSecond = unitsPerSecondInitial;
+            StartCoroutine(LerpUnitsPerSecondRoutine());
+        }
+
+        private void Update() => gameObject.transform.Rotate(0, 0, -unitsPerSecond * Time.deltaTime);
+
+        private IEnumerator LerpUnitsPerSecondRoutine()
+        {
+            while (true)
+            {
+                timeToLerp = Random.Range(minTimeToLerp, maxTimeToLerp);
+                while (unitsPerSecond <= unitsPerSecondFinal)
+                {
+                    Debug.Log("Aumentando");
+                    unitsPerSecond = Mathf.Lerp(unitsPerSecond, unitsPerSecondFinal, timeToLerp * Time.deltaTime);
+
+                    yield return null;
+                }
+
+                timeToLerp = Random.Range(minTimeToLerp, maxTimeToLerp);
+                while (unitsPerSecond >= unitsPerSecondInitial)
+                {
+                    Debug.Log("Disminuyendo");
+                    unitsPerSecond = Mathf.Lerp(unitsPerSecond, unitsPerSecondInitial, timeToLerp * Time.deltaTime);
+
+                    yield return null;
+                }
+
+                yield return new WaitForSeconds(stoppedTime);
+            }
         }
     }
 }
